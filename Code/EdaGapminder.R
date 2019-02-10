bcb7c28b2898185f5ab55f74c0dc67b17cb0e256
Pushkar B <pushkardps@gmail.com>
2019-02-10 20:45:10 +0530

    Create EdaGapminder.R

---
+++ Code/EdaGapminder.R
@@ -0,0 +1,8 @@
+gapminder <- read_csv("C:/Users/pushk/Desktop/pb/Harrisburg University/Exploratory Data Analysis/Git/dataanalytics/gapminder.csv")
+
+gap <- data.frame(gapminder)
+
+summary(gap)
+
+str(gap)
+
