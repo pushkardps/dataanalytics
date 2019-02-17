c963fdc71e653b355fa60b85e8c271242884abc9
Pushkar B <pushkardps@gmail.com>
2019-02-17 23:35:25 +0530

    Update EdaGapminder.R

--- Code/EdaGapminder.R
+++ Code/EdaGapminder.R
@@ -1,3 +1,5 @@
+
+#Import the file to be analyzed
 gapminder <- read_csv("C:/Users/pushk/Desktop/pb/Harrisburg University/Exploratory Data Analysis/Git/dataanalytics/gapminder.csv")
 
 gap <- data.frame(gapminder)
